93b21156cd952213c508c69da282dedd1d82c769
wuleiaty <1046828876@qq.com>
2019-03-26 12:34:55 +0800

    add apply

---
+++ src/apply.c
@@ -0,0 +1,11 @@
+//
+// Created by wulei on 19-3-26.
+//
+// Implement of apply module
+
+#include "header/object.h"
+#include "header/apply.h"
+
+void apply(object* procedure, object* arguments) {
+
+}
---
+++ src/header/apply.h
@@ -0,0 +1,12 @@
+//
+// Created by wulei on 19-3-26.
+//
+
+#include "object.h"
+
+#ifndef SCHEME_APPLY_H
+#define SCHEME_APPLY_H
+
+extern void apply(object* procedure, object* arguments);
+
+#endif //SCHEME_APPLY_H
