7b262b6871eece0fe2cf3b2c4d4b79c43570fadd
wuleiaty <1046828876@qq.com>
2019-03-26 21:25:29 +0800

    add built in functions

---
+++ src/builtin.c
@@ -0,0 +1,40 @@
+//
+// Created by wulei on 19-3-26.
+//
+// built in procedures and objects
+
+#include "header/builtin.h"
+#include "header/object.h"
+
+void init_built_in() {
+    true_obj = alloc_object(); /* init true_obj */
+    true_obj->type = BOOLEAN;
+    true_obj->data.boolean.value = true;
+
+    false_obj = alloc_object(); /* init false_obj */
+    false_obj->type = BOOLEAN;
+    false_obj->data.boolean.value = false;
+
+    the_empty_list = alloc_object(); /* init the_empty_list */
+    the_empty_list->type = THE_EMPTY_LIST;
+    symbol_table = the_empty_list;
+
+    quote_symbol  = make_symbol("quote" );
+    define_symbol = make_symbol("define");
+    set_symbol    = make_symbol("set!"  );
+    ok_symbol     = make_symbol("ok"    );
+    if_symbol     = make_symbol("if"    );
+    lambda_symbol = make_symbol("lambda");
+    begin_symbol  = make_symbol("begin" );
+    cond_symbol   = make_symbol("cond"  );
+    else_symbol   = make_symbol("else"  );
+    let_symbol    = make_symbol("let"   );
+    and_symbol    = make_symbol("and"   );
+    or_symbol     = make_symbol("or"    );
+
+    the_empty_environment = the_empty_list;
+//TODO
+// make the global environment, include init the env
+// and binding the builtin procedure to env
+
+}
---
+++ src/header/builtin.h
@@ -0,0 +1,42 @@
+//
+// Created by wulei on 19-3-26.
+//
+// built in procedures and objects
+
+#ifndef SCHEME_BUILTIN_H
+#define SCHEME_BUILTIN_H
+
+#include "object.h"
+
+#define caar(obj)   car(car(obj))
+#define cadr(obj)   car(cdr(obj))
+#define cdar(obj)   cdr(car(obj))
+#define cddr(obj)   cdr(cdr(obj))
+#define caaar(obj)  car(car(car(obj)))
+#define caadr(obj)  car(car(cdr(obj)))
+#define cadar(obj)  car(cdr(car(obj)))
+#define caddr(obj)  car(cdr(cdr(obj)))
+#define cdaar(obj)  cdr(car(car(obj)))
+#define cdadr(obj)  cdr(car(cdr(obj)))
+#define cddar(obj)  cdr(cdr(car(obj)))
+#define cdddr(obj)  cdr(cdr(cdr(obj)))
+#define caaaar(obj) car(car(car(car(obj))))
+#define caaadr(obj) car(car(car(cdr(obj))))
+#define caadar(obj) car(car(cdr(car(obj))))
+#define caaddr(obj) car(car(cdr(cdr(obj))))
+#define cadaar(obj) car(cdr(car(car(obj))))
+#define cadadr(obj) car(cdr(car(cdr(obj))))
+#define caddar(obj) car(cdr(cdr(car(obj))))
+#define cadddr(obj) car(cdr(cdr(cdr(obj))))
+#define cdaaar(obj) cdr(car(car(car(obj))))
+#define cdaadr(obj) cdr(car(car(cdr(obj))))
+#define cdadar(obj) cdr(car(cdr(car(obj))))
+#define cdaddr(obj) cdr(car(cdr(cdr(obj))))
+#define cddaar(obj) cdr(cdr(car(car(obj))))
+#define cddadr(obj) cdr(cdr(car(cdr(obj))))
+#define cdddar(obj) cdr(cdr(cdr(car(obj))))
+#define cddddr(obj) cdr(cdr(cdr(cdr(obj))))
+
+extern void init_built_in();
+
+#endif //SCHEME_BUILTIN_H
