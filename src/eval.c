e4c74f3b265b2be99003fd676cee851427908650
wuleiaty <1046828876@qq.com>
2019-03-26 12:35:29 +0800

    update eval

--- src/eval.c
+++ src/eval.c
@@ -1,14 +1,22 @@
 //
 // Created by wulei on 19-3-14.
+//
 // Implement of evaluate module
 
 #include "header/object.h"
 #include "header/eval.h"
 #include "header/environment.h"
+#include "header/apply.h"
+#include "header/error.h"
+#include "header/read.h"
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 object* eval(object* exp, object* env) {
+
     /* deal with this cases */
+
     if(is_self_evaluating(exp)) {
         return exp;
     }
@@ -33,48 +41,69 @@ object* eval(object* exp, object* env) {
                               env);
     }
     else if(is_begin(exp)) {
-
+        eval_sequence(begin_actions(exp), env);
+    }
+    else if(is_cond(exp)) {
+        eval(cond_to_if(exp), env);
+    }
+    else if(is_application(exp)) {
+        apply(eval(operator(exp), env),
+                list_of_values(operands(exp), env));
+    }
+    else {
+        error_handle_with_object(stderr,
+                                 "Unknown expression type --EVAL",
+                                 EXIT_FAILURE,
+                                 exp);
     }
 }
 
 bool is_self_evaluating(object* exp) {
-
+    return is_fixnum(exp) || is_string(exp) ? true : false;
 }
 
 bool is_variable       (object* exp) {
-
+    return is_symbol(exp);
 }
 
 bool is_quoted         (object* exp) {
-
+    return is_tagged_list(exp, quote_symbol);
 }
 
 bool is_assignment     (object* exp) {
-
+    return is_tagged_list(exp, set_symbol);
 }
 
 bool is_definition     (object* exp) {
-
+    return is_tagged_list(exp, define_symbol);
 }
 
 bool is_if             (object* exp) {
-
+    return is_tagged_list(exp, if_symbol);
 }
 
 bool is_lambda         (object* exp) {
-
+    return is_tagged_list(exp, lambda_symbol);
 }
 
 bool is_begin          (object* exp) {
-
+    return is_tagged_list(exp, begin_symbol);
 }
 
 bool is_cond           (object* exp) {
-
+    return is_tagged_list(exp, cond_symbol);
 }
 
 bool is_application    (object* exp) {
+    return is_pair(exp);
+}
 
+bool is_tagged_list(object* exp, object* tag) {
+    if(is_pair(exp)) {
+        object* car_exp = car(exp);
+        return is_symbol(car_exp) && car_exp == tag;
+    }
+    return false;
 }
 
 object* text_of_quotation(object* exp) {
@@ -102,5 +131,29 @@ object* lambda_parameters(object* exp) {
 }
 
 object* lambda_body(object* exp) {
+
+}
+
+void eval_sequence(object* exp, object* env) {
+
+}
+
+object* begin_actions(object* exp) {
+
+}
+
+object* cond_to_if(object* exp) {
+
+}
+
+object* operator(object* exp) {
+
+}
+
+object* operands(object* exp) {
+
+}
+
+object* list_of_values(object* exps, object* env) {
 
 }
--- src/header/eval.h
+++ src/header/eval.h
@@ -30,6 +30,8 @@ extern bool is_cond           (object* exp);
 
 extern bool is_application    (object* exp);
 
+extern bool is_tagged_list    (object* exp, object* tag);
+
 /****** more ******/
 
 extern object* text_of_quotation(object* exp);
@@ -45,5 +47,18 @@ extern object* make_procedure(object* parameters, object* body, object* env);
 extern object* lambda_parameters(object* exp);
 
 extern object* lambda_body(object* exp);
+
+extern void    eval_sequence(object* exp, object* env);
+
+extern object* begin_actions(object* exp);
+
+extern object* cond_to_if(object* exp);
+
+extern object* operator(object* exp);
+
+extern object* operands(object* exp);
+
+extern object* list_of_values(object* exps, object* env);
+
 
 #endif //SCHEME_EVAL_H
