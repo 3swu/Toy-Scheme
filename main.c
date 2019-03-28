284b74ef9eaefc94958f88925cf4651c85110312
wuleiaty <1046828876@qq.com>
2019-03-28 22:19:32 +0800

    update test

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <memory.h>
 
 #include "src/header/read.h"
+#include "src/header/builtin.h"
 
 int main() {
     char* buf = buf_pre_handle(read(stdin));
@@ -9,6 +10,8 @@ int main() {
     token* t = gen_token(buf);
     for(token* p = t; p != NULL; p = p->next)
         printf("%s\n", p->value);
+
+    init_built_in();
 
     token_list* list = gen_token_list(t);
     object* obj = parse(list);
