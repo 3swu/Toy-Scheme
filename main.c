7459753c54e8b1212a88e104c7638dc61ecd7ec5
wuleiaty <1046828876@qq.com>
2019-04-10 11:23:11 +0800

    update interface

--- main.c
+++ main.c
@@ -7,31 +7,34 @@
 #include "src/header/environment.h"
 #include "src/header/write.h"
 
-object* testfun(object* arguments){
-    return alloc_object();
-}
-
 int main() {
 //    init_built_in();
 //    object* obj = lookup_variable_value(make_symbol("+"), the_global_environment);
 //
 //    write(stdout, obj);
-    char* buf = buf_pre_handle(read(stdin));
-    printf("%s\n", buf);
-    token* t = gen_token(buf);
-    for(token* p = t; p != NULL; p = p->next)
-        printf("%s\n", p->value);
+//    char* buf = buf_pre_handle(read(stdin));
+//    printf("%s\n", buf);
+//    token* t = gen_token(buf);
+//    for(token* p = t; p != NULL; p = p->next)
+//        printf("%s\n", p->value);
 
     init_built_in();
 
-    token_list* list = gen_token_list(t);
-    object* obj = parse(list);
+//    token_list* list = gen_token_list(t);
+//    object* obj = parse(list);
+    object* obj;
     init_built_in();
-    object* result = lookup_variable_value(make_symbol("+"), the_global_environment);
-    write(stdout, result);
-    fflush(stdout);
-    object* o = eval(obj, the_global_environment);
-    write(stdout, o);
+    printf("Welcome to Scheme 0.1.0\n");
+    printf("Press Ctrl-C to exit\n");
+    for(; ;) {
+        printf("> ");
+        obj = reader(stdin);
+        if(obj == NULL)
+            break;
+        write(stdout, eval(obj, the_global_environment));
+        printf("\n");
+    }
+
     return 0;
 }
 
